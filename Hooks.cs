a72d906cf09193b6d0aeb500ed37c0eabd90f12e
David <dcasillas@gpinet.com>
2024-01-19 14:23:57 -0500

    Initial Commit

---
+++ Hooks.cs
@@ -0,0 +1,109 @@
+﻿using DllExporterNet4;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Projectwise_Hooks
+{
+	public static class HookSampleManagedClass
+	{
+		const int IDOK = 1;
+
+		public static HookCallback Hooks = new HookCallback();
+
+		public static PWWrapper.GenericHookFunction CreateFolderPreHook;
+		public static PWWrapper.GenericHookFunction ModifyFolderPreHook;
+
+		[DllExport]
+		public static int CustomInitialize(int ulMask, IntPtr lpReserved)
+		{
+			CreateFolderPreHook = Hooks.LoginPreHook;
+			PWWrapper.aaApi_AddHook((int)PWWrapper.HookIdentifiers.AAHOOK_CREATE_PROJECT, PWWrapper.HookTypes.AAPREHOOK, CreateFolderPreHook);
+			return IDOK;
+		}
+	}
+
+	public class HookCallback
+	{
+		public unsafe int LoginPreHook
+		(
+			int hookId,
+			int hookType,
+			[In, Out] IntPtr aParam1,
+			int aParam2,
+			ref int pResult
+		)
+		{
+			//_AAPROCREATE_PARAM pProjectParam = (_AAPROCREATE_PARAM)Marshal.PtrToStructure(aParam1, typeof(_AAPROCREATE_PARAM));
+			//string name = pProjectParam.lptstrName;
+			//if (name.Length > 20)
+			//{
+			//	MessageBox.Show("Folder name can not have more than 20 characters.", "Error Creating Folder");
+			//	return (int)PWWrapper.HookActions.AAHOOK_ERROR;
+			//}
+
+			return (int)PWWrapper.HookActions.AAHOOK_SUCCESS;
+		}
+
+		/// <summary>
+		/// The AAPOSTHOOK hook type specifies that the hook handler function is being registered to be called
+		/// after successful processing of the event action.
+		/// All post-hooks should return AAHOOK_SUCCESS.
+		/// Currently, the return values of post-hooks are ignored, and the return value from the hook handler
+		/// function cannot be used to prevent the execution of any other post-hook handlers in the chain.
+		/// To register a handler to be called regardless of the success or failure of the event action,
+		/// register a Post-Fail Hook.
+		/// </summary>
+		/// <param name="hookId">The id of the hook that is being called.</param>
+		/// <param name="hookType">Type type of hook -Pre, Action, or Post.</param>
+		/// <param name="aParam1">Varies by hook Id. ALWAYS review the documentation!</param>
+		/// <param name="aParam2">Varies by hook Id. ALWAYS review the documentation!</param>
+		/// <param name="pResult">Varies by hook Id. ALWAYS review the documentation!</param>
+		/// <returns>Varies by hook type.</returns>
+		public unsafe int LoginPostHook
+		(
+			int hookId,
+			int hookType,
+			[In, Out] IntPtr aParam1,
+			int aParam2,
+			ref int pResult
+		)
+		{
+			// "best practice" is it check to see if the value of aParam2 is one of the types you are interested in
+			// otherwise just let the hook continue.
+			// aParam2 for AAHOOK_LOGIN can be one of three values.
+			// The only one in our case that we care about is AAOPER_USER_LOGIN.
+			//if (aParam2 != (int)HookParamValue.AAOPER_USER_LOGIN)
+			//{
+			//    return (int)PWWrapper.HookActions.AAHOOK_SUCCESS;  // let the hook chain continue
+			//}
+
+			// "best practice" is to cast aParam1 for the structure/type for this particular hook.
+			// Note that the cast type can depend upon the VALUE of aParam2!
+			// In this case, since we only care about AAOPER_USER_LOGIN,
+			// we will cast it to an AALOGIN_PARAM structure.
+			// For this posthook, we will only use the username...
+			//_AALOGIN_PARAM* pLoginParam = (_AALOGIN_PARAM*)aParam1;
+
+			// There may be other ways to get to the strings in the structure
+			// but this way works...
+			//string userName = Marshal.PtrToStringAuto((IntPtr)pLoginParam->lpctstrUser);
+			//string userName = "dc";
+			//StringBuilder sbDSN = new StringBuilder(1024);
+			//PWWrapper.aaApi_GetActiveDatasourceName(sbDSN, sbDSN.Capacity);
+			//DialogResult result = MessageBox.Show($"Logged in to '{sbDSN}'' as '{userName}'",
+			//    "Post Login Message",
+			//    MessageBoxButtons.OK);
+			//    MessageBoxButtons.OK);
+
+			//_AAPROCREATE_PARAM* pProjectParam = (_AAPROCREATE_PARAM*)aParam1;
+			_AAPROCREATE_PARAM pProjectParam = (_AAPROCREATE_PARAM)Marshal.PtrToStructure(aParam1, typeof(_AAPROCREATE_PARAM));
+			//int folderId = pProjectParam->lProjectId;
+			//string folderName = pProjectParam->lptstrName;
+
+			MessageBox.Show($"About to create folder.\n{pProjectParam.lProjectId} -> {pProjectParam.lptstrName} {aParam2} {pResult}");
+			//MessageBox.Show($"Folder created.\n{folderId} -> {folderName} {aParam2} {pResult}");
+			return (int)PWWrapper.HookActions.AAHOOK_SUCCESS;
+		}
+	}
+}
---
+++ ParamStructures.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projectwise_Hooks
+{
+	public static class ParamStructures
+	{
+		// For AAHOOK_LOGIN
+		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+		public unsafe struct _AALOGIN_PARAM
+		{
+			/// <summary>
+			/// [in] Reserved for future use.  Must be 0.
+			/// </summary>
+			[MarshalAs(UnmanagedType.U4)]
+			public UInt32 ulMask;
+
+			/// <summary>
+			/// [in] Deprecated. Specifies the data source type. Pass 0.
+			/// </summary>
+			public int IDsType;
+
+			/// <summary>
+			/// [in] Pointer to null-terminated string specifying the data source name.
+			/// </summary>
+			[MarshalAs(UnmanagedType.LPWStr)]
+			public char* lpctstrDSource;
+
+			/// <summary>
+			/// [in] Pointer to null-terminated string specifying the user name. This field will be NULL if SSO is used to authenticate the user.
+			/// </summary>
+			[MarshalAs(UnmanagedType.LPWStr)]
+			public char* lpctstrUser;
+
+			/// <summary>
+			/// [in] Pointer to null-terminated string specifying the password. This field will be NULL if SSO is used to authenticate the user.
+			/// </summary>
+			[MarshalAs(UnmanagedType.LPWStr)]
+			public char* lpctstrPassword;
+
+			/// <summary>
+			/// [in] Deprecated. Specifies the schema file. Pass NULL.
+			/// </summary>
+			[MarshalAs(UnmanagedType.LPWStr)]
+			public char* lpctstrSchema;
+		}
+
+		// For AAHOOK_CREATE_PROJECT
+		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+		public unsafe struct _AAPROCREATE_PARAM
+		{
+			// Reserved for future use.  Must be 0.
+			[MarshalAs(UnmanagedType.U4)]
+			public uint ulMask;
+
+			// Specifies the project Id for the new project.
+			public int lProjectId;
+
+			// Specifies the Id of the storage to create the project in. 
+			public int lStorageId;
+
+			// Specifies the project owner's account Id.
+			public int lManagerId;
+
+			// Specifies new project's type.
+			public int lType;
+
+			// Pointer to a null-terminated string specifying the project name.
+			[MarshalAs(UnmanagedType.LPWStr)]
+			public string lptstrName;
+
+			// Pointer to a null-terminated string specifying the project name.
+			[MarshalAs(UnmanagedType.LPWStr)]
+			public string lptstrDesc;
+
+			// Pointer to a null-terminated string specifying the project name.
+			[MarshalAs(UnmanagedType.LPWStr)]
+			public string lptstrConfig;
+
+			// Specifies the parent project identifier.
+			public int lParentId;
+
+			// Specifies the source project Id to base the new project on.
+			public int lSourceId;
+
+			// Specifies the workflow identifier to be assigned to the new project.
+			public int lWorkflowId;
+
+			// Specifies the identifier of the project that access control to copy for the new project.
+			public int lAccessProject;
+
+			// Specifies the environment identifier of the project.
+			public int lEnvironmentId;
+
+			// Specifies the type of the account assigned as project's owner (member lManagerId).
+			public int lManagerType;
+
+			// Specifies the id of workspace profile.
+			public int lWorkspaceProfileId;
+
+			// Specifies the pointer to ODS instance object that contains rich project properties.
+			public IntPtr pComponentInstance;
+
+			// Specifies the class id of the ODS instance object that contains rich project properties.
+			public int lComponentClassId;
+
+			// Specifies the class id of the ODS instance object that contains rich project properties.
+			public int lComponentInstanceId;
+
+			// Indicates that a new ODS instance should be created for the rich project properties, and the supplied one is only a template.
+			public bool cloneComponentInstance;
+
+			// Specifies valid bits in projFlags.
+			[MarshalAs(UnmanagedType.U4)]
+			public uint projFlagMask;
+
+			// Specifies the project flags.
+			[MarshalAs(UnmanagedType.U4)]
+			public uint projFlags;
+		}
+	}
+}
